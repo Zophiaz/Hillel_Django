7a2839c97fe1c19833c738ea954a3e37858c535c
Zophiaz <melissa.kix87@gmail.com>
2023-02-25 21:14:47 +0200

    amend

--- shopping_list/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('/', views.index, name='index'),
-    path('add/', views.add_item, name='add_item'),
-    path('<item_id>/buy', views.buy_item, name='buy_item'),
-    path('<item_id>/remove', views.remove_item, name='remove_item'),
-]
