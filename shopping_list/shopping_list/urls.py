5f45a775c604578c535eb1db86f27d5f75deae18
Zophiaz <melissa.kix87@gmail.com>
2023-02-25 21:10:20 +0200

    18. Django, база данних

--- shopping_list/shopping_list/urls.py
+++ shopping_list/shopping_list/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#import shopping_list.polls.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shopping_list/', include('polls.urls')),
+    path('add/', include('polls.urls')),
+    path('buy/', include('polls.urls')),
+    path('remove/', include('polls.urls')),
 ]
