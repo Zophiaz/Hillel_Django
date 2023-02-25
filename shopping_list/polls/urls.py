31ec924b5801c2a70a74e3dec44a4b0dbe69dcbd
Zophiaz <melissa.kix87@gmail.com>
2023-02-25 21:17:25 +0200

    ДЗ 17. Отримуємо данні

---
+++ shopping_list/polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/', views.index, name='index'),
+    path('add/', views.add_item, name='add_item'),
+    path('<item_id>/buy', views.buy_item, name='buy_item'),
+    path('<item_id>/remove', views.remove_item, name='remove_item'),
+]
